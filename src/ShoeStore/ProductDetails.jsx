29ca27314bb33bbf93618c0691ca9d8e13bb8f12
StevenTran <trantrongtinnna3k55@gmail.com>
2022-07-24 20:10:20 +0700

    deployed modal

--- src/ShoeStore/ProductDetails.jsx
+++ src/ShoeStore/ProductDetails.jsx
@@ -1,41 +1,72 @@
-import React from 'react'
+import React from "react";
 
-const ProductDetails = ({product}) => {
+const ProductDetails = ({ product, isOpen = false, onClose }) => {
   if (!product) {
-    return null
+    return null;
   }
 
-    return (
-    <div className = "row mt-5">
-        <div className = "col-sm-6">
-            <img src = {product.image} alt = {product.name}/>
-        </div>
-        <div className = "col-sm-6">
-            <h1> Shoe's Detail</h1>
-           <table className = "table">
-                <tbody>
-                    <tr>
-                        <td>Name</td>
-                        <td>{product.name}</td>
-                    </tr>
-                    <tr>
-                        <td>Price</td>
-                        <td>{product.price}</td>
-                    </tr>
-                    <tr>
-                        <td>Description</td>
-                        <td>{product.description}</td>
-                    </tr>
-                    <tr>
-                        <td>Quantity</td>
-                        <td>{product.quantity}</td>
-                    </tr>
-                 
-                </tbody>
-           </table>
-        </div>
-    </div>
-  )
-}
+  return (
+    <>
+    <div className='modal fade show modal-lg' style = {{ display : isOpen ? 'block' : 'none'}} tabindex='-1' role='dialog'>
+      <div className='modal-dialog' role='document'>
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <h5 className='modal-title'>Shoe's detail</h5>
+            <button
+              type='button'
+              className='close'
 
-export default ProductDetails
+               onClick = {onClose}
+            >
+              <span aria-hidden='true'>&times;</span>
+            </button>
+          </div>
+          <div className='modal-body'>
+            <div className='row mt-5'>
+              <div className='col-sm-6'>
+                <img src={product.image} alt={product.name} width = '100%' />
+              </div>
+              <div className='col-sm-6'>
+                <h1> Shoe's Detail</h1>
+                <table className='table'>
+                  <tbody>
+                    <tr>
+                      <td>Name</td>
+                      <td>{product.name}</td>
+                    </tr>
+                    <tr>
+                      <td>Price</td>
+                      <td>{product.price}</td>
+                    </tr>
+                    <tr>
+                      <td>Description</td>
+                      <td>{product.description}</td>
+                    </tr>
+                    <tr>
+                      <td>Quantity</td>
+                      <td>{product.quantity}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div className='modal-footer'>
+         
+            <button
+              type='button'
+              className='btn btn-secondary'
+              onClick = {onClose}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    { isOpen && <div className = 'modal-backdrop fade show'></div>}
+    </>
+  );
+};
+
+export default ProductDetails;
--- src/ShoeStore/ShoeStore.jsx
+++ src/ShoeStore/ShoeStore.jsx
@@ -8,19 +8,29 @@ class ShoeStore extends React.Component {
    
      this.state = {
         selectedProduct : null ,
+        isOpen : false,
      }
    }
+  handleOpen = () => {
+    this.setState({isOpen:true })
+  }
+  handleClose = () => {
+    this.setState({ isOpen : false})
+  }
+
   handleSelect = (product) => {
     console.log(product)
     this.setState( {selectedProduct : product})
+    this.setState({isOpen:true })
   }
+
   render () {
     const {selectedProduct} = this.state
     return (
       <div>
           <h1 className = "text-center text-danger">Shoe Store</h1>
           <ProductList products = {data} onSelect = {this.handleSelect}/>
-          <ProductDetails product = {selectedProduct} />
+          <ProductDetails isOpen = {this.state.isOpen} onClose = {this.handleClose} product = {selectedProduct} />
       </div>
     )
   }
